cd24c3e177745b58ef84c99d63e374d883fb5be0
yjin-desktop <yjinlee99@gmail.com>
2024-06-23 01:31:26 +0900

    first push

---
+++ src/main/java/com/isle/isleGame/IsleGameApplication.java
@@ -0,0 +1,13 @@
+package com.isle.isleGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsleGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsleGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isle/isleGame/IsleGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isle.isleGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsleGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
